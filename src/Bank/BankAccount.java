c949e1482561093abf738619d5c90d98aaf47f98
Ivo Neiman <contactoivoneiman@gmail.com>
2024-12-25 20:42:41 -0300

    Refactored BankAccount class to centralize minimum balance validations and added validation for negative deposits.

--- src/Bank/BankAccount.java
+++ src/Bank/BankAccount.java
@@ -10,22 +10,29 @@ public class BankAccount implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	String name;
+	private String name;
 	private double balance;
-	double min_balance;
-	String acc_num;
+	private double min_balance;
+	private String acc_num;
 	//String type;
 	
 	
-	public BankAccount(String name, double balance, double min_balance) {
-		this.name = name;
-		this.balance = balance;
-		this.min_balance = min_balance;
-		acc_num = 10000 + (int)(Math.random()*89999) + "";
-	}
+	public BankAccount(String name, double balance, double min_balance) throws Exception {
+    if (balance < min_balance) {
+        throw new Exception("Initial balance cannot be less than the minimum required balance: " + min_balance);
+    }
+    this.name = name;
+    this.balance = balance;
+    this.min_balance = min_balance;
+    this.acc_num = 10000 + (int) (Math.random() * 89999) + "";
+}
 
-	public void deposit(double amount)
+
+	public void deposit(double amount) throws InvalidAmount
 	{
+		if (amount <= 0){
+			throw new InvalidAmount("Deposit amount must be greater than zero.")
+		}
 		balance+=amount;
 	}
 	
